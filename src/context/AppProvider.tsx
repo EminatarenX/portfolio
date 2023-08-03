914e40a79a7db89b2b18ca829545bc7117499101
EminatarenX <eminataren2002@gmail.com>
2023-08-02 19:23:45 -0600

    anadiendo contador de visitas

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+import axios from "axios";
 
 const AppContext = createContext({
   darkMode: true,
@@ -12,6 +14,31 @@ interface AppProviderProps {
 
 const  AppProvider = ({ children }: AppProviderProps) => {
   const [darkMode, setDarkMode] = useState(true)
+  const [visitas, setVisitas] = useState('')
+
+
+
+
+  useEffect(()=> {
+    const getVisitas = async () => {
+      try {
+        const { data } = await axios.get('/api/visitas')
+        setVisitas(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    const postVisitas = async () => {
+      try {
+       await axios.post('/api/visitas', {})
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getVisitas()
+    postVisitas()
+  },[])
 
   return (
     <AppContext.Provider 
